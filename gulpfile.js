587c415b7e277fa52d8627fc120d008ae87b577d
Olya <osokolova1@gmail.com>
2019-05-16 13:58:21 +0300

    Отладка сборки:источника svg-спрайта, перезагрузки сервера при изменениях контента

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
+var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -31,7 +32,7 @@ gulp.task("css", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src("source/img/sprite/*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
@@ -63,18 +64,49 @@ gulp.task("webp", function() {
     .pipe(gulp.dest("build/img"));
 });
 
+gulp.task("copy", function(){
+  return gulp.src([
+    "sourrce/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+  });
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/"/*,
     notify: false,
     open: true,
     cors: true,
-    ui: false
+    ui: false*/
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/sprite/*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/img/*.{svg,png,jpg}", gulp.series("refresh"));
+  gulp.watch("source/js/*.js", gulp.series("refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
-gulp.task("build", gulp.series("css", "sprite", "html"));
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
+));
+
 gulp.task("start", gulp.series("build", "server"));
